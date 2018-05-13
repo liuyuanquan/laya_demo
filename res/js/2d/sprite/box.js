bf64af7603f20e8d596f6c4a3b8f195657fc2567
liuyuanquan <liuyuanquan@babytree-inc.com>
2018-05-13 23:31:11 +0800

    sprite-容器

---
+++ res/js/2d/sprite/box.js
@@ -0,0 +1,40 @@
+Laya.init(Laya.Browser.width, Laya.Browser.height);
+
+// Laya.stage.scaleMode = 'showall';
+Laya.stage.alignH = 'center';
+Laya.stage.alignV = 'middle';
+Laya.stage.bgColor = "#232628";
+
+var imgs = [
+	baseUrl + 'monkey0.png',
+	baseUrl + 'monkey1.png',
+	baseUrl + 'monkey2.png',
+	baseUrl + 'monkey3.png'
+]
+
+var x = Laya.Browser.width >> 1;
+var y = Laya.Browser.height >> 1;
+var r = 150;
+var deg = Math.PI / 2;
+
+Laya.loader.load(imgs, Laya.Handler.create(null, function(flag) {
+	if (flag) {
+		var box = new Laya.Sprite();
+		box.graphics.drawCircle(0, 0, r, '#ff0000');
+
+		for(var i = 0;i < imgs.length;i++) {
+			var res = Laya.loader.getRes(imgs[i]);
+			var img = new Laya.Sprite();
+			img.graphics.drawTexture(res);
+			img.pivot(res.sourceWidth >> 1, res.sourceHeight >> 1).pos(Math.cos(deg * i) * r, -Math.sin(deg * i) * r);
+			box.addChild(img);
+		}	
+
+		box.pos(x, y).scale(2, 2).rotation = Math.atan(x / y) * 180 / Math.PI;
+		Laya.timer.frameLoop(1, box, function() {
+			this.rotation += 1;
+		}, null, true);
+		Laya.stage.addChild(box);
+			
+	}
+}));
--- res/js/2d/sprite/showimg.js
+++ res/js/2d/sprite/showimg.js
@@ -1,18 +1,21 @@
-Laya.init(750, 1334);
+Laya.init(Laya.Browser.width, Laya.Browser.height);
 
 Laya.stage.scaleMode = 'showall';
+Laya.stage.alignH = 'center';
+Laya.stage.alignV = 'middle';
+Laya.stage.bgColor = "#232628";
 
-// 加载图片方式1
+// 加载图片方式1ls
 var img1 = new Laya.Sprite();
-img1.loadImage(baseUrl + 'xujie.jpg', 0, 0, 375, 375, Laya.Handler.create(null, function() {
+img1.loadImage(baseUrl + 'xujie.jpg', 0, 0, 376, 376, Laya.Handler.create(null, function() {
 	Laya.stage.addChild(img1);
-})).pivot(375 / 2, 0).pos(375, 100);
+})).pivot(138, 0).pos(Laya.Browser.width >> 1, 100);
 
 // 加载图片方式2
 Laya.loader.load(baseUrl + 'zhuiming.jpg', Laya.Handler.create(null, function(res) {
 	var img2 = new Laya.Sprite();
 	img2.graphics.drawTexture(res, 0, 0, 375, 375 * res.sourceHeight / res.sourceWidth);
-	img2.pivot(375 / 2, img2.getBounds().height).pos(375, 1234);
+	img2.pivot(138, img2.getBounds().height).pos(Laya.Browser.width >> 1, Laya.Browser.height - 100);
 	Laya.stage.addChild(img2)
 }));
 
